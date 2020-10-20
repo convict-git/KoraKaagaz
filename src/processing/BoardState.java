f7799b669589c4b54178044ff6689b646c4a2500
himanshu2047 <111701013@smail.iitpkd.ac.in>
2020-10-20 23:18:31 +0530

    Add comments

--- src/processing/BoardState.java
+++ src/processing/BoardState.java
@@ -6,25 +6,21 @@ import processing.utility.*;
 import processing.board_object.*;
 
 /**
+* This BoardState class stores two maps which should be serializable for persistance.
+* ClientBoardState and ServerBoardState will store the object of this class in their class.
 *
 * @author Himanshu Jain
 */
 
 public class BoardState implements Serializable {
 
-	/**
-	 *  To authenticate that the user deserializing this object.
-	 */
+	//To authenticate that the user deserializing this object.
 	private static final long serialVersionUID = 1L;
 	
-	/** 
-	 * Map from object ID to the Board Object
-	 */
+	 //Map from object ID to the Board Object
 	private Map <ObjectId, BoardObject> objIdToBoardObject;
 	
-	/**
-	 *  Map from position on the board to Priority of objects on that position
-	 */
+	// Map from position on the board to Priority of objects on that position
 	private Map <Position, PriorityQueue <PriorityQueueObject>> pixelToObjects;
 	
 	//constructor to initialize both the maps
--- src/processing/ClientBoardState.java
+++ src/processing/ClientBoardState.java
@@ -5,15 +5,14 @@ import processing.board_object.*;
 import processing.utility.*;
 
 /**
+* This class contains all the info of the current board.
 *
 * @author Himanshu Jain
 */
 
 public class ClientBoardState {
 	
-	/**
-	 * maps is an object of the BoardState containing both the maps
-	 */
+	//maps is an object of the BoardState containing both the maps
 	public static BoardState maps = new BoardState();
 	
 	//port Number of the Board Server
@@ -29,4 +28,8 @@ public class ClientBoardState {
 	
 	//to store the selected object
 	public static PriorityQueueObject selectedObject;
+	
+	//to store the username and userId
+	public static Username username;
+	public static UserId userId;
 }
