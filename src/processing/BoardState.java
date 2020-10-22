640d15eb272c1095f20911debb8b90170822d329
Shruti Umat <111701027@smail.iitpkd.ac.in>
2020-10-23 01:03:20 +0530

    Remove helper functions from BoardState class as they will be implemented in SelectDeleteUtil class

--- src/processing/BoardState.java
+++ src/processing/BoardState.java
@@ -6,75 +6,26 @@ import processing.utility.*;
 import processing.board_object.*;
 
 /**
-* This BoardState class stores two maps which should be serializable for persistance.
+* This BoardState class stores two maps which should be serializable for persistence.
 * ClientBoardState and ServerBoardState will store the object of this class in their class.
 *
-* @author Himanshu Jain
+* @author Himanshu Jain, Shruti Umat
 */
 
 public class BoardState implements Serializable {
 
-	//To authenticate that the user deserializing this object.
+	// To authenticate that the user deserializing this object.
 	private static final long serialVersionUID = 1L;
-	
-	 //Map from object ID to the Board Object
+
+	// Map from object ID to the Board Object
 	private Map <ObjectId, BoardObject> objIdToBoardObject;
-	
+
 	// Map from position on the board to Priority of objects on that position
 	private Map <Position, PriorityQueue <PriorityQueueObject>> pixelToObjects;
-	
-	//constructor to initialize both the maps
+
+	// Constructor to initialize both the maps
 	public BoardState() {
 		objIdToBoardObject = new HashMap <ObjectId, BoardObject> ();
 		pixelToObjects = new HashMap <Position, PriorityQueue <PriorityQueueObject>> ();
-	}
-	
-	//get the Board Object given objectId
-	public BoardObject getObject(ObjectId objectId) {
-		if(objIdToBoardObject.containsKey(objectId)) {
-			return objIdToBoardObject.get(objectId);
-		} else {
-			return null;
-		}
-	}
-	
-	//get the priority queue given the position
-	public PriorityQueue<PriorityQueueObject> getObject(Position pos) {
-		if(pixelToObjects.containsKey(pos)) {
-			return pixelToObjects.get(pos);
-		} else {
-			return null;
-		}
-	}
-	
-	//insert a Board Object with corresponding objectId
-	public void insertObject(ObjectId objectId, BoardObject obj) {
-		objIdToBoardObject.put(objectId,  obj);
-	}
-	
-	//insert an object in the priority queue
-	public void insertObject(Position pos, PriorityQueueObject obj) {
-		if(pixelToObjects.containsKey(pos)) {
-			pixelToObjects.get(pos).add(obj);
-		} else {
-			PriorityQueue <PriorityQueueObject> pq = new PriorityQueue <PriorityQueueObject> (20, new ObjectComparator());
-			pq.add(obj);
-			pixelToObjects.put(pos, pq);
-		}
-	}
-	
-	//delete an object from the objIdToBoardObject map
-	public void deleteObject(ObjectId objectId) {
-		if(objIdToBoardObject.containsKey(objectId)) {
-			objIdToBoardObject.remove(objectId);
-		}
-		return;
-	}
-	
-	//delete an object from the priority queue
-	public void deleteObject(Position pos, PriorityQueueObject obj) {
-		if(pixelToObjects.containsKey(pos)) {
-			pixelToObjects.get(pos).remove(obj);
-		}
 	}
 }
