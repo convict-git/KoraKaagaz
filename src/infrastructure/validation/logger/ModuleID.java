bb30368e63699160c843871fe251cc1320fdf238
Navaneeth M Nambiar <nmnambiar@phoenix>
2020-10-23 22:01:29 +0530

    add enums used by ILogger

---
+++ src/infrastructure/validation/logger/ModuleID.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package infrastructure.validation.logger;
+
+/**
+ * @author Navaneeth M Nambiar
+ *
+ */
+
+public enum ModuleID {
+	NETWORKING,
+	PROCESSING,
+	UI,
+	INFRASTRUCTURE
+}
