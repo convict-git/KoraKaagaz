f8020c31045107674c37ed5d7b02deb44c11a175
himanshu2047 <111701013@smail.iitpkd.ac.in>
2020-10-24 17:32:50 +0530

    Add IP address in UserId

--- src/processing/utility/UserId.java
+++ src/processing/utility/UserId.java
@@ -11,8 +11,8 @@ public class UserId {
 	
 	// Build the User ID using the IP Address and
 	// the username
-	public UserId(String ipAddress, Username username) {
-		userId = ipAddress + "_" + username.toString();
+	public UserId(IpAddress ipAddress, Username username) {
+		userId = ipAddress.toString() + "_" + username.toString();
 	}
 	
 	// Convert User ID to String
