45afe5a66a397c2ea7dce8ea0fe747e8fa93c822
srikar <srikarreddymadaka@gmail.com>
2020-10-24 16:32:22 +0530

    skeleton of LanCommunicator

---
+++ src/networking/LanCommunicator.java
@@ -0,0 +1,23 @@
+package networking;
+/**
+ *
+ * @author Madaka Srikar Reddy
+ */
+
+import queueManagement.*;
+
+class LanCommunicator implements ICommunicator{
+
+    public void start(){
+        return;
+    }
+
+    public void stop(){
+        return;
+    }
+
+    public void send(String destination, String message, String identifier){
+        return;
+    }
+
+}
