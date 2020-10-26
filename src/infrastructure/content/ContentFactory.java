3b7c574c4fd9dcc356a020238316a0816db74260
badalgeebin <badalgeebin@gmail.com>
2020-10-26 22:27:21 +0530

    worked on proper alignment of code

--- src/infrastructure/content/ContentFactory.java
+++ src/infrastructure/content/ContentFactory.java
@@ -7,16 +7,16 @@ public final class ContentFactory {
 	private ContentFactory() {}
 	
     public static IContentCommunicator getContentCommunicator() {
-		if (instance1 == null) {
+    	if (instance1 == null) {
 			instance1 = new ContentCommunicator();
 		}
-        return instance1;
+		return instance1;
     }
     
     public static IServerPort getServerPort() {
-		if (instance2 == null) {
+    	if (instance2 == null) {
 			instance2 = new ServerPort();
 		}
-        return instance2;
+    	return instance2;
     }
 }
