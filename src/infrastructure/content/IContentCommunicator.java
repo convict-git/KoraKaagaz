4a099c9b8ad4cd484fafb93ed7e2c215ade3137a
badalgeebin <badalgeebin@gmail.com>
2020-10-26 22:47:41 +0530

    resolved some issues

--- src/infrastructure/content/IContentCommunicator.java
+++ src/infrastructure/content/IContentCommunicator.java
@@ -5,18 +5,18 @@ public interface IContentCommunicator {
 	 * UI will call this method with one json formatted string argument
 	 * @param : userDetails - It is json string which will contain server ip_address, username, image
 	 */
-	void initialiseUser (String userDetails);
+	void initialiseUser(String userDetails);
 	
 	/*
 	 * UI will call this to paas the message of client to everyone else
 	 * @param : message - it is the actual message as a string
 	 */
-	void sendMessageToContent (String message);
+	void sendMessageToContent(String message);
 	
 	/*
 	 * UI will call this to notify that the current user is quitting the session to all other users
 	 */
-	void notifyUserExit ();
+	void notifyUserExit();
 	
 	/*
 	 * UI will subscribe to us in order to receive any updates coming from other users
