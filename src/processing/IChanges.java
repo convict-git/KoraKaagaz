c3337c0be9aa9aa438ca4e283b95eaace3c5b83f
himanshu2047 <111701013@smail.iitpkd.ac.in>
2020-10-26 17:05:18 +0530

    sove conflicts

---
+++ src/processing/IChanges.java
@@ -0,0 +1,16 @@
+package processing;
+
+import java.util.*;
+import processing.utility.*;
+
+/**
+ * 
+ * @author Himanshu Jain
+ *
+ */
+
+public interface IChanges {
+	
+	void getChanges(ArrayList <Pixel> pixels);
+
+}
