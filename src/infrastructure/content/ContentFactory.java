faa73ea13c08cf3d229c82fa4a655384b8e0cefe
badalgeebin <badalgeebin@gmail.com>
2020-10-26 20:38:21 +0530

    modified the code completely

--- src/infrastructure/content/ContentFactory.java
+++ src/infrastructure/content/ContentFactory.java
@@ -1,9 +1,22 @@
-package content;
+package infrastructure.content;
 
 public final class ContentFactory {
+	private static IContentCommunicator instance1;
+	private static IServerPort instance2;
+	
+	private ContentFactory() {}
 	
     public static IContentCommunicator getContentCommunicator() {
-        IContentCommunicator communicator = new ContentCommunicator();
-        return communicator;
+		if (instance1 == null) {
+			instance1 = new ContentCommunicator();
+		}
+        return instance1;
+    }
+    
+    public static IServerPort getServerPort() {
+		if (instance2 == null) {
+			instance2 = new ServerPort();
+		}
+        return instance2;
     }
 }
