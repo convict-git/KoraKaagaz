d0544a16467381bf060f90f70588f455b34fdb4d
Talha Yaseen <khantalhakhan94@gmail.com>
2020-10-26 22:51:20 +0530

    Package name changed

--- src/infrastructure/content/IContentNotificationHandler.java
+++ src/infrastructure/content/IContentNotificationHandler.java
@@ -4,8 +4,7 @@ public interface IContentNotificationHandler {
 	// Meta field is username of a new user
 	void onNewUserJoined (String username);
 	
-	/* Meta fields of JSON file messageDetails are 
-	userid, message and image */
+	// Meta fields of JSON file messageDetails are userid, message and image
 	void onMessageReceived (String messageDetails);
 	
 	// Meta field is username of a new user
