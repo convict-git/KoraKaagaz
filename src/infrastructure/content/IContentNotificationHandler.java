2a50f8ea7b5daf027c979d59bab0190178511c3b
Talha Yaseen <khantalhakhan94@gmail.com>
2020-10-26 22:32:48 +0530

    IContentNotificationHandler Interface

--- src/infrastructure/content/IContentNotificationHandler.java
+++ src/infrastructure/content/IContentNotificationHandler.java
@@ -4,7 +4,8 @@ public interface IContentNotificationHandler {
 	// Meta field is username of a new user
 	void onNewUserJoined (String username);
 	
-	// Meta fields of JSON file messageDetails are userid, message and image
+	/* Meta fields of JSON file messageDetails are 
+	userid, message and image */
 	void onMessageReceived (String messageDetails);
 	
 	// Meta field is username of a new user
