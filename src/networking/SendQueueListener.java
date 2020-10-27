3d02b6fed49df4b06b244b1289bfd18b272b682e
sirpa sahul <sirpasahul343@gmail.com>
2020-10-27 12:38:03 +0530

    added exception handling code

--- src/networking/SendQueueListener.java
+++ src/networking/SendQueueListener.java
@@ -34,7 +34,9 @@ public class SendQueueListener implements Runnable {
                 dout.close();
                 sock.close();
             }
-            
+            catch(Exception e){
+                System.out.println(e);
+            }
         }
     }
 }
