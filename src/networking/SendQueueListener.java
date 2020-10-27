93224f2d9baf9bf0ee02df7d720f6c24f5eb6712
sirpa sahul <sirpasahul343@gmail.com>
2020-10-27 12:26:36 +0530

    extracted ip address and port information from destination address

--- src/networking/SendQueueListener.java
+++ src/networking/SendQueueListener.java
@@ -2,11 +2,14 @@ package networking;
 import networking.queueManagement.*;
 import networking.utility.*;
 public class SendQueueListener implements Runnable {
+    public String [] splitAddress(String destination){
+        return destination.split(":");
+    }
     public void run(){
         
         while(LanCommunicator.isRunning){
             OutgoingPacket out = LanCommunicator.SendQueue.front();
-            String IP = out.destination;
+            String [] destination = this.splitAddress(out.destination);
             String message = out.message;
             String identifier = out.identifier;
             if(identifier == "processing"){
