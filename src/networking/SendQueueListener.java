cd2459b8e877b5c7de12a0686c9df4b05ced11c2
sirpa sahul <sirpasahul343@gmail.com>
2020-10-27 12:07:35 +0530

    extracted information from queue

--- src/networking/SendQueueListener.java
+++ src/networking/SendQueueListener.java
@@ -1,7 +1,20 @@
 package networking;
-
+import networking.queueManagement.*;
+import networking.utility.*;
 public class SendQueueListener implements Runnable {
     public void run(){
         
+        while(LanCommunicator.isRunning){
+            OutgoingPacket out = LanCommunicator.SendQueue.front();
+            String IP = out.destination;
+            String message = out.message;
+            String identifier = out.identifier;
+            if(identifier == "processing"){
+                message = "p" + message;
+            }
+            else if(identifier == "content"){
+                message = "c" + message;
+            }
+        }
     }
 }
