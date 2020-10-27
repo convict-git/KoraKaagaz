55048bc743b7eec059eca237f0263ada780eade8
Talha Yaseen <khantalhakhan94@gmail.com>
2020-10-28 00:09:14 +0530

    Comments updated

--- src/infrastructure/content/IContentNotificationHandler.java
+++ src/infrastructure/content/IContentNotificationHandler.java
@@ -9,7 +9,7 @@ public interface IContentNotificationHandler {
 	
 	/*
 	 * Content will call this method with one json formatted string argument to pass the details to UI of every clients
-	 * @param : messageDetails - It is a json string whose meta fields are userid, message and image
+	 * @param : messageDetails - It is a json string whose meta fields are username (identifer of the user who sent the message), message (actual message sent by user), image (profile picture of user who sent the message) and time (the time the user sent this message) 
 	 */
 	void onMessageReceived (String messageDetails);
 	
