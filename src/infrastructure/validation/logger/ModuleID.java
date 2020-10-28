c2d644ef4f97a8c7569116035aa44e59a5e2255f
Navaneeth M Nambiar <nmnambiar@phoenix>
2020-10-28 12:02:55 +0530

    added comments describing enums

--- src/infrastructure/validation/logger/ModuleID.java
+++ src/infrastructure/validation/logger/ModuleID.java
@@ -1,11 +1,15 @@
-/**
- * 
- */
 package infrastructure.validation.logger;
 
 /**
+ * The ILogger interface depends on the ModuleID enum
+ * The ModuleID enum helps specify which module is calling the log
+ * Currently supported module names are:
+ *   NETWORKING     - the Networking module
+ *   PROCESSING     - the Processing module
+ *   UI             - the UI module
+ *   INFRASTRUCTURE - the Infrastructure module
+ *   
  * @author Navaneeth M Nambiar
- *
  */
 
 public enum ModuleID {
