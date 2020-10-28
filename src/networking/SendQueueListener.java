9124825ed79aee0e1c4e0ad1fd18788da7d31a1e
sirpa sahul <sirpasahul343@gmail.com>
2020-10-28 22:50:09 +0530

    small changes to the logic

--- src/networking/SendQueueListener.java
+++ src/networking/SendQueueListener.java
@@ -57,65 +57,76 @@ public class SendQueueListener implements Runnable {
         // run the while loop as long as the application is running.
         while(LanCommunicator.isRunning){
             
-            //Take the outgoing packet from the sendqueue.
-            OutgoingPacket out = LanCommunicator.SendQueue.front();
+            //Check whether queue is empty or not, if it's not empty, 
+            //then try to send the message.
+            if( !LanCommunicator.SendQueue.isEmpty() ) {
 
-            //Divide the destination address into ip and port from destination.
-            String[] dest = this.splitAddress(out.destination);
+                //Take the outgoing packet from the sendqueue.
+                OutgoingPacket out = LanCommunicator.SendQueue.front();
 
-            //Take ip address from the dest array
-            String ip = dest[0];
+                //Divide the destination address into ip and port from destination.
+                String[] dest = this.splitAddress(out.destination);
 
-            //take port number from the dest array and parse it into integer.
-            int port = Integer.parseInt(dest[1]);
+                //Take ip address from the dest array
+                String ip = dest[0];
 
+                //take port number from the dest array and parse it into integer.
+                int port = Integer.parseInt(dest[1]);
+
+                //if port number or ip address is invalid then no need to send the message
+                //simply discard it by contiuing to the next message.
+                if(!isValidIpaddress(ip) || !isValidPort(port)){
+                    //as the message is invalid simply dequeue and continue for next one.
+                    LanCommunicator.SendQueue.dequeue();
+                    continue;
+                }
+                
+                //store the message into the message variable
+                String message = out.message;
             
-            
-            //store the message into the message variable
-            String message = out.message;
-        
-            //store the identifier variable into the identifier.
-            String identifier = out.identifier;
+                //store the identifier variable into the identifier.
+                String identifier = out.identifier;
 
-            //Encode the identifier into the message itself.
-            if(identifier == "processing"){
-                message = "p" + message;
+                //Encode the identifier into the message itself.
+                if(identifier == "processing"){
+                    message = "p" + message;
+                }
+                else if(identifier == "content"){
+                    message = "c" + message;
+                }
+                else{
+                    continue;
+                }
+
+                //The following code in try block will try to send the message over the network.
+                try{
+
+                    //create socket using ip address and port number.
+                    Socket sock = new Socket(ip, port);
+
+                    //create data output stream as we are using tcp.
+                    DataOutputStream dout = new DataOutputStream(sock.getOutputStream());
+
+                    //encode the data into UTF format and write it to output stream
+                    dout.writeUTF(message);
+
+                    //flush the byte stream into the network.
+                    dout.flush();
+
+                    //close the outgoing stream.
+                    dout.close();
+
+                    //now close the socket.
+                    sock.close();
+                } catch (Exception e) {
+                    
+                    //if any exception occurs then we are printing the error to console.
+                    System.out.println(e);
+                }
+                
+                //Now dequeue the message from sendqueue.
+                LanCommunicator.SendQueue.dequeue();
             }
-            else if(identifier == "content"){
-                message = "c" + message;
-            }
-            else{
-                continue;
-            }
-
-            //The following code in try block will try to send the message over the network.
-            try{
-
-                //create socket using ip address and port number.
-                Socket sock = new Socket(ip, port);
-
-                //create data output stream as we are using tcp.
-                DataOutputStream dout = new DataOutputStream(sock.getOutputStream());
-
-                //encode the data into UTF format and write it to output stream
-                dout.writeUTF(message);
-
-                //flush the byte stream into the network.
-                dout.flush();
-
-                //close the outgoing stream.
-                dout.close();
-
-                //now close the socket.
-                sock.close();
-            } catch (Exception e) {
-
-                //if any exception occurs then we are printing the error to console.
-                System.out.println(e);
-            }
-            
-            //Now dequeue the message from sendqueue.
-            LanCommunicator.SendQueue.dequeue();
         }
     }
 }
