366915514ccc26172b02dd5bfec7cd7694778f6f
Navaneeth M Nambiar <nmnambiar@phoenix>
2020-10-28 12:02:55 +0530

    added comments describing enums

--- src/infrastructure/validation/logger/LogLevel.java
+++ src/infrastructure/validation/logger/LogLevel.java
@@ -1,11 +1,15 @@
-/**
- * 
- */
 package infrastructure.validation.logger;
 
 /**
+ * The ILogger interface depends on the LogLevel enum
+ * The LogLevel enum is used to specify the type of log used:
+ *   1. ERROR   - a significant error has occurred and the program can halt
+ *   2. WARNING - a warning that might indicate possible fault
+ *   3. SUCCESS - a successful completion of a particular procedure
+ *   4. INFO    - an informative message. Common example would be start/end
+ *        of a procedure/transaction
+ * 
  * @author Navaneeth M Nambiar
- *
  */
 public enum LogLevel {
 	ERROR,
