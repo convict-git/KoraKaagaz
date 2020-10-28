eb1ddab990fee5a0c61f66316c0e34d246ba1df4
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2020-10-28 23:54:45 +0530

    Updates to BrushRadius

--- src/processing/utility/BrushRadius.java
+++ src/processing/utility/BrushRadius.java
@@ -1,9 +1,32 @@
 package processing.utility;
 
+/**
+ * Class Representing a Brush Radius
+ *
+ * @author Ahmed Zaheer Dadarkar
+ * @reviewer Rakesh Kumar, Himanshu Jain
+ */
+
 public class BrushRadius {
-	public double brushRadius; // The Brush Radius
+
+	/** The Brush Radius */
+	public double brushRadius; 
 	
-	// The equals method
+	/**
+ 	 * BrushRadius Constructor
+ 	 * 
+ 	 * @param brushRadius Brush Radius as an double value
+ 	 */
+ 	public BrushRadius(double brushRadius) {
+ 		this.brushRadius = brushRadius;
+ 	}
+
+ 	/** Copy Constructor */
+ 	public BrushRadius(BrushRadius brushRadObj) {
+ 		brushRadius = brushRadObj.brushRadius;
+ 	}
+	
+	/* The equals method */
 	@Override
 	public boolean equals(Object obj) {
 		if(obj instanceof BrushRadius)
