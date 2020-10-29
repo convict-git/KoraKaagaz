116cdbef620c58d581c1bd502e3cc95540ecab2c
Prudhvis-gitrepo <111701023@smail.iitpkd.ac.in>
2020-10-29 18:27:41 +0530

    commented

--- src/networking/utility/ClientInfo.java
+++ src/networking/utility/ClientInfo.java
@@ -3,13 +3,21 @@ package networking.utility;
 /**
 * 
 * @author Prudhvi Vardhan Reddy Pulagam
+* 
+* @ClientInfo class consists of information about respective individual
+* client's IP addres and Port number on which the application listening too. 
+*
+* @getIp() method returns client's IP address as String and takes no arguments.
+* @getPort() method returns client's Port number as integer and takes no arguments
+*.
 */
+
 public class ClientInfo{
 
 	public String getIp(){
 		return "";
 	}
-	
+
 	public int getPort(){
 		return 0;
 	}
