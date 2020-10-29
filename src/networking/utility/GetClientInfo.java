f2c5ba110d5962329db851947e68a9d8e75a66fb
Prudhvis-gitrepo <111701023@smail.iitpkd.ac.in>
2020-10-29 17:29:16 +0530

    added new feature to communicatorfactory class

---
+++ src/networking/utility/GetClientInfo.java
@@ -0,0 +1,11 @@
+package networking.utility;
+public class GetClientInfo{
+	private String ip="HI";
+	private int port=10;
+	public String getIp(){
+		return ip;
+	}
+	public int getPort(){
+		return port;
+	}
+}
