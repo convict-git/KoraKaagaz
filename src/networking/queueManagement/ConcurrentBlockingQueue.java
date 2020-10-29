f77b34d574c9d363cf99bd129fb3b54be996f374
shiva-santhosh <shivasanthosh20999@gmail.com>
2020-10-29 21:11:07 +0530

    Added Comments

--- src/networking/queueManagement/ConcurrentBlockingQueue.java
+++ src/networking/queueManagement/ConcurrentBlockingQueue.java
@@ -3,52 +3,98 @@ package networking.queueManagement;
 import java.util.concurrent.*;
 
 /**
+ *  This file contains the class ConcurrentBlockingQueue which implements the the queue interface
+ *  IQueue.This class can be used to create a Queue Data-Structure which is used to store and also retrieve
+ *  the data in FIFO (First In First Out) manner.
+ *
+ *  To create a Queue, one can declare
+ *  IQueue<T> q = new ConcurrentBlockingQueue<T>();
+ *  where T is of any builtin or user-defined data-type/structure/class.
  *
  * @author Pandravisam Shiva Santhosh
  */
 
 public class ConcurrentBlockingQueue<T> implements IQueue<T> {
+
+    /**
+     * It contains the maximum number of elements that queue can store.
+     */
     int capacity;
+
+    /**
+     * Declaring the the java inbuilt Blocking Queue which is thread-safe and will  not rise the concurrency issues
+     */
     BlockingQueue<T> q;
 
-    ConcurrentBlockingQueue(){
+    /**
+     * Constructor which initialises capacity and creates a ArrayBlocking queue instance of size equal to
+     *  capacity.
+     */
+    public ConcurrentBlockingQueue(){
         capacity = 100000;
         q = new ArrayBlockingQueue<T>(capacity);
     }
 
-    public void enqueue(T item){
-        try {
-            q.put(item);
-        } catch (InterruptedException e){
-            //throws System.out.println("Got Interrupted");
-        }
+    /**
+     *  This method "clear" removes all the elements present in the queue
+     */
+    public void clear(){
+        q.clear();
     }
 
+    /**
+     * This method "dequeue" takes no argument and removes the item which has been there for long time in the queue.
+     * In other words removes the front item in the queue.
+     */
     public void dequeue(){
         try {
             T item = q.take();
         } catch (InterruptedException e){
-            //System.out.println("Got Interrupted");
+            System.out.println("The ex");
+            /**/
         }
     }
 
+    /**
+     * This method "enqueue" takes a argument item and it inserts at the tail of the queue.
+     * This method doesn't return anything.
+     *
+     * @param item the data that will go in the queue.
+     */
+    public void enqueue(T item){
+        try {
+            q.put(item);
+        } catch (InterruptedException e){
+            //throws System.out.println("Exception has been raised");
+        }
+    }
+
+    /**
+     *This method "front" takes no argument and returns the front item of the queue without
+     * removing the item from the queue
+     *
+     * @return the front item if queue is not empty or else returns NULL
+     */
     public T front(){
-        T frontItem = q.peek();
-        return frontItem;
+        return q.peek();
     }
 
+    /**
+     * This method "isEmpty" takes no argument and returns a boolean value indicating true if the
+     *  queue is empty and false if the queue is not empty.
+     *
+     * @return true/false depending on the condition.
+     */
     public boolean isEmpty(){
-        if(q.size() == 0){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return (q.size() == 0);
     }
 
+    /**
+     *  This method "size" takes no argument and returns a integer indicating the size of the Queue
+     *
+     * @return current size of the queue
+     */
     public int size(){
-        int queueSize = q.size();
-        return queueSize;
+        return q.size();
     }
-
 }
--- src/networking/utility/IncomingPacket.java
+++ src/networking/utility/IncomingPacket.java
@@ -1,51 +1,62 @@
 package networking.utility;
 
 /**
+ * This file contains the class that is going to used by SockListener-Component for adding the data
+ * to the queue and ReceiveQueueListener-Component for retrieving the data from the queue.
  *
  * @author Pandravisam Shiva Santhosh
 */
 
-/*
-    This file contains the class that is going to used by SockListener-Component for adding the data
-    to the queue and ReceiveQueueListener-Component for retrieving the data from the queue.
- */
-
 public class IncomingPacket{
 
-    // The content of the message that needs to be delivered to the corresponding module
+    /**
+     * The content of the message that needs to be delivered to the corresponding module
+     */
     String message;
 
-    // Empty Constructor, can be used to instantiate the object and later assign the values to the variables in the class
+    /**
+     * Empty Constructor, can be used to instantiate the object and later assign the values to the
+     * variables in the class
+     */
     public IncomingPacket(){
     }
 
-    // Overloaded Constructor which initialises all the variables in the class
+    /**
+     *  Overloaded Constructor which initialises all the variables in the class
+     */
     public IncomingPacket(String message){
         this.message = message;
     }
 
-    //This method "setMessage" sets the value of message content that needs to delivere
+    /**
+     * This method "setMessage" sets the value of message content that needs to delivered
+     *
+     * @param message the string that is needed be assigned to the message variable in the class.
+     */
     public void setMessage(String message){
         this.message = message;
     }
 
-    // This method "getMessage" returns the message
+    /**
+     * This method "getMessage" returns the content in the message variable.
+     *
+     * @return the content in the message variable of the class.
+     */
     public String getMessage(){
         return message;
     }
 
-    /*
-    This utility method takes a object as a argument and returns true if both the objects are equal or
-    else returns false
+    /**
+     * This utility method "isEqual" takes a object as a argument and returns true if both the objects are equal or
+     * else returns false
+     *
+     * @param obj any object.
+     *
+     * @return true, if the current class object equals to the object that was as a argument or else false.
     */
     public boolean isEqual(Object obj) {
         if(obj instanceof IncomingPacket){
-            if(message == ((IncomingPacket) obj).message) {
-                return true;
-            }
-            else {
-                return false;
-            }
+            return (message == ((IncomingPacket) obj).message);
         }
         else {
             return false;
