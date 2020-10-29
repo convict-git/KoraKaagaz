7d74cd32a022b7956736b73ccae19c3105a308dd
Prudhvis-gitrepo <111701023@smail.iitpkd.ac.in>
2020-10-29 18:09:17 +0530

    changed names and cleaned up unwanted

--- src/networking/utility/ClientInfo.java
+++ src/networking/utility/ClientInfo.java
@@ -4,10 +4,12 @@ package networking.utility;
 * 
 * @author Prudhvi Vardhan Reddy Pulagam
 */
-public class GetClientInfo{
+public class ClientInfo{
+
 	public String getIp(){
 		return "";
 	}
+	
 	public int getPort(){
 		return 0;
 	}
