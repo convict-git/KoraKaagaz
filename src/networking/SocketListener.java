b80090c22d264808e9a11aa2a7b9cd698d9dc65c
Marella Shiva Sai Teja <shivasai962@gmail.com>
2020-10-31 17:32:52 +0530

    Add few comments

--- src/networking/SocketListener.java
+++ src/networking/SocketListener.java
@@ -13,37 +13,88 @@ import networking.utility.IncomingPacket;
 
 public class SocketListener implements Runnable {
 	
+	/**
+	 * Port number on which the socket will be listening for the client requests to connect with.
+	 */
 	int port;
+
+	/**
+	 * socket object which keeps listening for the client requests.
+	 */
 	private ServerSocket serverSocket;
+
+	/**
+	 * Queue into which message from content module will be pushed
+	 */
 	private ConcurrentBlockingQueue<IncomingPacket> contModuleQueue;
+
+	/**
+	 * Queue into which message from processing module will be pushed
+	 */
 	private ConcurrentBlockingQueue<IncomingPacket> procModuleQueue;
 
+	/**
+	 * 
+	 * This method is the constructor of the class which initializes the params
+	 * @param port
+	 * @param contModuleQueue
+	 * @param procModuleQueue
+	 * 
+	 * There won't be any return type as it is a constructor of the class
+	 */
     public SocketListener(int port, ConcurrentBlockingQueue<IncomingPacket> contModuleQueue, ConcurrentBlockingQueue<IncomingPacket> procModuleQueue){
+		
 		this.port = port;
 		this.contModuleQueue = contModuleQueue;
 		this.procModuleQueue = procModuleQueue;
+
     }
 
+	/**
+	 * This method parses the identifier within packet received from the client
+	 * @param packet	Data received from the client
+	 * @return	Identifier
+	 */
+
 	public String getIdFromPacket(String packet){
-		// convert String to char[] array
+
+		/**
+		 * converts String to char[] array
+		 */
         char[] chars = packet.toCharArray();
+		
+		/**
+		 * String Builder to build a string for the identifier
+		 */
 		StringBuilder idBuilder = new StringBuilder();
-        // iterate over char[] array using enhanced for loop
+		
+		/**
+		 * Iterates over char[] array and appends to the Identifier Builder
+		 */
         for (char ch : chars) {
             if(ch == ':'){
 				break;
 			}
 			idBuilder.append(ch);
 		}
+
+		/**
+		 * Converts the String Builder to String
+		 */
 		String id = idBuilder.toString();
+
 		return id;
+
 	}
 
 	public String getMsgFromPacket(String packet){
-		// convert String to char[] array
+
         char[] chars = packet.toCharArray();
 		StringBuilder idBuilder = new StringBuilder();
-		// iterate over char[] array using enhanced for loop
+
+		/**
+		 * A Utility Boolean flag 
+		 */
 		boolean flag = false;
         for (char ch : chars) {
 			if(flag){
@@ -55,6 +106,7 @@ public class SocketListener implements Runnable {
 		}
 		String id = idBuilder.toString();
 		return id;
+
 	}
 
     public void push(String id, String msg) {
