793b993bc9051f0acf7d978302313847f5860297
shiva-santhosh <42898234+shiva-santhosh@users.noreply.github.com>
2020-11-01 19:04:37 +0530

    #48-Final main branch (#27)
    * Added Comments
    
    * Added Comments
    
    * Added Comments
    
    * Added identifier in Incoming Packets
    
    * Added Comments
    
    * Small fix
    
    * Small fix
    
    * Small fix
    
    * Small fix
    
    * Small fix
    
    * Logging is been done
    
    * Logging is been done

--- src/networking/queueManagement/ConcurrentBlockingQueue.java
+++ src/networking/queueManagement/ConcurrentBlockingQueue.java
@@ -1,54 +1,109 @@
 package networking.queueManagement;
 
+import infrastructure.validation.logger.*;
 import java.util.concurrent.*;
 
+
 /**
+ *  This file contains the class ConcurrentBlockingQueue which implements the the queue interface
+ *  IQueue.This class can be used to create a Queue Data-Structure which is used to store and also retrieve
+ *  the data in FIFO (First In First Out) manner.
+ *
+ *  To create a Queue, one can declare
+ *  IQueue<T> q = new ConcurrentBlockingQueue<T>();
+ *  where T is of any builtin or user-defined data-type/structure/class.
  *
  * @author Pandravisam Shiva Santhosh
  */
 
 public class ConcurrentBlockingQueue<T> implements IQueue<T> {
+
+    /**
+     * It contains the maximum number of elements that queue can store.
+     */
     int capacity;
+
+    /**
+     * Declaring the the java inbuilt Blocking Queue which is thread-safe and will  not rise the concurrency issues
+     */
     BlockingQueue<T> q;
 
-    ConcurrentBlockingQueue(){
+    /**
+     *  Logger object from the LoggerFactory to log Messages.
+     */
+    Ilogger logger = LoggerFactory.getLoggerInstance();
+
+    /**
+     * Constructor which initialises capacity and creates a ArrayBlocking queue instance of size equal to
+     *  capacity.
+     */
+    public ConcurrentBlockingQueue(){
         capacity = 100000;
         q = new ArrayBlockingQueue<T>(capacity);
+        logger.log(ModuleID.NETWORKING, LogLevel.SUCCESS, "A new queue has been constructed which is thread-safe and blocking");
     }
 
-    public void enqueue(T item){
-        try {
-            q.put(item);
-        } catch (InterruptedException e){
-            //throws System.out.println("Got Interrupted");
-        }
+    /**
+     *  This method "clear" removes all the elements present in the queue
+     */
+    public void clear(){
+        q.clear();
     }
 
+    /**
+     * This method "dequeue" takes no argument and removes the item which has been there for long time in the queue.
+     * In other words removes the front item in the queue.
+     */
     public void dequeue(){
         try {
             T item = q.take();
         } catch (InterruptedException e){
-            //System.out.println("Got Interrupted");
+            logger.log(ModuleID.NETWORKING, LogLevel.ERROR, "Raised in QueueManagement because a interruption is caused while the queue is waiting");
         }
     }
 
+    /**
+     * This method "enqueue" takes a argument item and it inserts at the tail of the queue.
+     * This method doesn't return anything.
+     *
+     * @param item the data that will go in the queue.
+     */
+    public void enqueue(T item){
+        try {
+            q.put(item);
+        } catch (InterruptedException e){
+            logger.log(ModuleID.NETWORKING, LogLevel.ERROR, "Raised in QueueManagement because a interruption is caused while the queue is waiting");
+        } catch(NullPointerException e){
+            logger.log(ModuleID.NETWORKING, LogLevel.ERROR, "Raised in QueueManagement because the element inserted is NULL which should not be inserted");
+        }
+    }
+
+    /**
+     *This method "front" takes no argument and returns the front item of the queue without
+     * removing the item from the queue
+     *
+     * @return the front item if queue is not empty or else returns NULL
+     */
     public T front(){
-        T frontItem = q.peek();
-        return frontItem;
+        return q.peek();
     }
 
+    /**
+     * This method "isEmpty" takes no argument and returns a boolean value indicating true if the
+     *  queue is empty and false if the queue is not empty.
+     *
+     * @return true/false depending on the condition.
+     */
     public boolean isEmpty(){
-        if(q.size() == 0){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return (q.size() == 0);
     }
 
-    public int size(){
-        int queueSize = q.size();
-        return queueSize;
+    /**
+     *  This method "size" takes no argument and returns a integer indicating the size of the Queue
+     *
+     * @return current size of the queue
+     */
+    public int size() {
+        return q.size();
     }
-
 }
--- src/networking/utility/OutgoingPacket.java
+++ src/networking/utility/OutgoingPacket.java
@@ -1,80 +1,114 @@
 package networking.utility;
 
 /**
+ * This file contains the class that is going to used by Send-Component for adding the data to the queue
+ * and SendQueueListener-Component for retrieving the data from the queue.
  *
  * @author Pandravisam Shiva Santhosh
  */
 
-/*
-    This file contains the class that is going to used by Send-Component for adding the data to the queue
-     and SendQueueListener-Component for retrieving the data from the queue.
- */
-
 public class OutgoingPacket {
 
-    // Destination IP address of the host for which the message should be delivered
+    /**
+     * Destination IP address of the host for which the message should be delivered
+     */
     String destination;
 
-    // The content of the message that needs to be delivered
+    /**
+     * The content of the message that needs to be delivered
+     */
     String message;
 
-    // To indicate which module is sending the message
+    /**
+     * To indicate which module is sending the message
+     */
     String identifier;
 
-    // Empty Constructor, can be used to instantiate the object and later assign the values to the variables in the class
+    /**
+     *  Empty Constructor, can be used to instantiate the object and later assign the values to the variables
+     *  in the class
+     */
     public OutgoingPacket(){
     }
 
-    // Overloaded Constructor which initialises all the variables in the class
+    /**
+     * Overloaded Constructor which initialises all the variables in the class
+     *
+     * @param destination  initialises the destination variable of the class
+     * @param message initialises the message variable of the class
+     * @param identifier initialises the identifier variable of the class
+     */
     public OutgoingPacket(String destination, String message, String identifier) {
         this.destination = destination;
         this.message = message;
         this.identifier = identifier;
     }
 
-    //This method "setDestination" sets the value destination IP address
+    /**
+     * This method "setDestination" sets the value destination IP address
+     *
+     * @param destination assigns the destination variable of the class
+     */
     public void setDestination(String destination){
         this.destination = destination;
     }
 
-    //This method "setMessage" sets the value of message content that needs to delivere
+    /**
+     * This method "setMessage" sets the value of message content that needs to delivere
+     *
+     * @param message assigns the message variable of the class
+     */
     public void setMessage(String message){
         this.message = message;
     }
 
-    //This method "setIdentifier" sets the value of the identifier.
+    /**
+     * This method "setIdentifier" sets the value of the identifier.
+     *
+     * @param identifier assigns the identifier variable in the class
+     */
     public void setIdentifier(String identifier){
         this.identifier = identifier;
     }
 
-    //This method "getDestination" returns the destination
+    /**
+     * This method "getDestination" returns the destination
+     *
+     * @return the destination variable content as a string
+     */
     public String getDestination(){
         return destination;
     }
 
-    // This method "getMessage" returns the message
+    /**
+     * This method "getMessage" returns the message
+     *
+     * @return the message variable content as a string
+     */
     public String getMessage(){
         return message;
     }
 
-    // This method "getIdentifier" returns the identifier
+    /**
+     * This method "getIdentifier" returns the identifier
+     *
+     * @return the identifier variable content as a string
+     */
     public String getIdentifier(){
         return identifier;
     }
 
-    /*
-    This utility method takes a object as a argument and returns true if both the objects are equal or
-    else returns false
+    /**
+     * This utility method "isEqual" takes a object as a argument and returns true if both the objects are equal or
+     * else returns false
+     *
+     * @param obj any object.
+     * @return true, if the current class object equals to the object that was as a argument or else false.
      */
     public boolean isEqual(Object obj) {
         if(obj instanceof OutgoingPacket){
-            if(destination == ((OutgoingPacket) obj).destination && message == ((OutgoingPacket) obj).message
-                    && identifier == ((OutgoingPacket) obj).identifier) {
-                return true;
-            }
-            else {
-                return false;
-            }
+            return ( destination == ((OutgoingPacket) obj).destination && message == ((OutgoingPacket) obj).message
+                    && identifier == ((OutgoingPacket) obj).identifier );
         }
         else {
             return false;
