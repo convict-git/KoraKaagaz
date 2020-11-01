464f7ce542117ba840b62e1c5e090642c80f5ac5
Navaneeth M Nambiar <nmnambiar@phoenix>
2020-11-01 22:13:19 +0530

    add dummy file logger and console logger

---
+++ src/infrastructure/validation/logger/ConsoleLogger.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package infrastructure.validation.logger;
+
+/**
+ * @author Navaneeth M Nambiar
+ */
+public class ConsoleLogger implements ILogger {
+
+	/**
+	 * 
+	 */
+	protected ConsoleLogger() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void log(ModuleID moduleIdentifier, LogLevel level, String message) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
