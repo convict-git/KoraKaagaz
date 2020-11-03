165880a4586d9b610687c0c68ae3756c01344159
himanshu2047 <111701013@smail.iitpkd.ac.in>
2020-11-03 22:06:09 +0530

    add giveSelectedPixels

--- src/processing/IChanges.java
+++ src/processing/IChanges.java
@@ -19,4 +19,13 @@ public interface IChanges {
 	 * @param pixels List of all the pixels where there is a change
 	 */
 	void getChanges(ArrayList<Pixel> pixels);
+	
+	/**
+	 * giveSelectedPixels will pass the list of pixels belonging to the
+	 * selected object, if the selected object got changed in between
+	 * by any other client.
+	 * 
+	 * @param pixels list of pixels belonging to the selected object
+	 */
+	void giveSelectedPixels(ArrayList<Pixel> pixels);
 }
