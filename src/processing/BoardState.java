1f29b751fc171437b154a739616b245b28946f8d
Shruti Umat <111701027@smail.iitpkd.ac.in>
2020-11-04 20:54:59 +0530

    Add getPixelsAtTop function in BoardState
    Add select delete operations
    
    Fetch latest changes in these files
    
    Pull IChanges from week1 branch
    
    Wrap lines and fix multiline comments
    
    Pull IChanges from master2
    
    Fix communicate changes error, coding style in BoardState

--- src/processing/BoardState.java
+++ src/processing/BoardState.java
@@ -15,50 +15,57 @@ import processing.boardobject.*;
 
 public class BoardState implements Serializable {
 
-	/** To authenticate that the user deserializing this object */
+	/* To authenticate that the user deserializing this object */
 	private static final long serialVersionUID = 1L;
 
-	/** Map from object ID to the Board Object */
+	/* Map from object ID to the Board Object */
 	private Map <ObjectId, BoardObject> objIdToBoardObject;
 
-	/** Map from position on the board to Priority Queue of objects on that position */
+	/* Map from position on the board to Priority Queue of objects on that position */
 	private Map <Position, PriorityQueue <PriorityQueueObject>> posToObjects;
 
-	/** Constructor to initialize both the maps */
+	/* Constructor to initialize both the maps */
 	public BoardState() {
 		objIdToBoardObject = new HashMap <ObjectId, BoardObject> ();
 		posToObjects = new HashMap <Position, PriorityQueue <PriorityQueueObject>> ();
 	}
 
-	/** Looks up BoardObject for the ObjectId key in the argument */
+	/* Looks up BoardObject for the ObjectId key in the argument */
 	public synchronized BoardObject getBoardObjectFromId(ObjectId objId) {
 		return objIdToBoardObject.get(objId);
 	}
 
 	/**
-	 * Gets ObjectIds at the top of Priority Queues at each position (x, y) in given ArrayList.
+	 * Gets ObjectIds at the top of Priority Queues at each position (x, y) in given ArrayList
 	 * The ObjectId that is present at the maximum number of positions i.e. which occurs
 	 * the maximum number of times is returned
 	 * This is done as the selected pixel positions can be such that
-	 * some of the pixels belong to an object and some to others.
+	 * some of the pixels belong to an object and some to others
+	 *
+	 * @param positions list of positions on the board
+	 * @return ObjectId present on top of maximum number of input positions
 	 */
 	public synchronized ObjectId getMostProbableObjectId(ArrayList <Position> positions) {
 		HashMap<ObjectId, Integer> count = new HashMap<ObjectId, Integer>();
 		for (Position pos: positions) {
-			PriorityQueue <PriorityQueueObject> queue_at_pos = posToObjects.get(pos);
-			if (queue_at_pos == null) {
+			PriorityQueue <PriorityQueueObject> queueAtPos = posToObjects.get(pos);
+			if (queueAtPos == null) {
 				continue;
 			}
-			PriorityQueueObject top_obj = (PriorityQueueObject) queue_at_pos.peek();
-			if (top_obj == null) {
+			PriorityQueueObject topPQobj = (PriorityQueueObject) queueAtPos.peek();
+			if (topPQobj == null) {
+				continue;
+			}
+			// there should be a valid object for this ObjectId
+			if (getBoardObjectFromId(topPQobj.objectId).isResetObject()) {
 				continue;
 			}
 
-			Integer curr = count.get(top_obj.objectId);
+			Integer curr = count.get(topPQobj.objectId);
 			if (curr == null) {
 				curr = 0;
 			}
-			count.put(top_obj.objectId, curr + 1);
+			count.put(topPQobj.objectId, curr + 1);
 		}
 
 		Integer maxOccurrence = 0;
@@ -74,6 +81,41 @@ public class BoardState implements Serializable {
 		}
 
 		return maxOccObjectId;
+	}
+
+	/**
+	 * Gets pixels at the top of the input positions
+	 *
+	 * @param positions positions on the board
+	 * @return pixels at these positions
+	 */
+	public synchronized ArrayList<Pixel> getPixelsAtTop (ArrayList<Position> positions) {
+		ArrayList<Pixel> topPixels = new ArrayList<Pixel>();
+		if (positions == null) {
+			return topPixels;
+		}
+
+		for (Position p : positions) {
+			PriorityQueue<PriorityQueueObject> queueAtPos = posToObjects.get(p);
+			if (queueAtPos == null) {
+				topPixels.add(new Pixel(p, new Intensity(255, 255, 255)));
+				continue;
+			}
+
+			PriorityQueueObject topPQobj = queueAtPos.peek();
+			if (topPQobj == null) {
+				topPixels.add(new Pixel(p, new Intensity(255, 255, 255)));
+			}
+			else {
+				BoardObject topObject = getBoardObjectFromId(topPQobj.objectId);
+				Pixel atThisPos = topObject.getPixels().get(0);
+				// Assuming that the object is uni-coloured and should have pixels
+
+				topPixels.add(new Pixel(p, atThisPos.intensity));
+			}
+		}
+		assert (positions.size() == topPixels.size());
+		return topPixels;
 	}
 
 	/**
@@ -134,12 +176,13 @@ public class BoardState implements Serializable {
 		return objIdToBoardObject.remove(objId);
 	}
 
-
 	/* APIs to Update maps to be used internally by other Processing Module classes */
 
 	/**
 	 * Reads the ObjectId, Timestamp, ArrayList<Position> fields of the input BoardObject
 	 * calls insertIntoPQ and insertObjectIntoMap to update both the maps
+	 *
+	 * @param boardObject board object to be inserted into the maps
 	 */
 	public void insertObjectIntoMaps (BoardObject boardObject) {
 		ObjectId objId = boardObject.getObjectId();
@@ -158,8 +201,9 @@ public class BoardState implements Serializable {
 	 * corresponding to this ObjectId
 	 * Reads the ArrayList <Position> and Timestamp fields from the BoardObject and calls removeFromPQ
 	 * Lastly, it calls removeObjectFromMap with the given ObjectId
-	 * The deleted BoardObject is passed for serialization if applicable
-	 * i.e. only if this was a local delete operation and needs to be sent to other clients via the server
+	 *
+	 * @param objectId objectId to be removed from the maps
+	 * @return a copy of the board object removed from the maps
 	 */
 	public BoardObject removeObjectFromMaps (ObjectId objectId) {
 		BoardObject remove = removeObjectFromMap(objectId);
@@ -168,7 +212,7 @@ public class BoardState implements Serializable {
 		}
 
 		Timestamp timestamp = remove.getTimestamp();
-		
+
 		ArrayList<Position> positions = remove.getPositions();
 
 		PriorityQueueObject pqRemoveObject = new PriorityQueueObject(objectId, timestamp);
@@ -179,8 +223,10 @@ public class BoardState implements Serializable {
 
 	/**
 	 * Looks up the BoardObject of the ObjectId using the map
-	 * From the BoardObject, UserId field is read
-	 * From UserId, username is extracted (after the '_')
+	 * From the BoardObject, user name is returned as String
+	 *
+	 * @param objId objectId of a BoardObject
+	 * @return username of the input object as a String
 	 */
 	public String getUserNameOfObject(ObjectId objId) {
 		BoardObject boardObject = objIdToBoardObject.get(objId);
