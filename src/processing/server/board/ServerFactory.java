526981596aba37d6a652378bc208e4d422db40bd
himanshu2047 <111701013@smail.iitpkd.ac.in>
2020-11-05 23:41:18 +0530

    Add comments

--- src/processing/server/board/ServerFactory.java
+++ src/processing/server/board/ServerFactory.java
@@ -11,10 +11,27 @@ package processing.server.board;
 
 public class ServerFactory {
 	
+	/**
+	 * clientIP is an object of class ClientIP which implements IClientIP
+	 * interface which has the function to give user details to content 
+	 * module. So a new instance of class ClientIP is created and saved in 
+	 * clientIP.
+	 */
 	public static ClientIP clientIP = new ClientIP();
 	
+	/**
+	 * Making the constructor as private so as none can create an instance of 
+	 * this class so as to make it a singleton pattern to send the same ClientIP
+	 * instance everytime.
+	 */
 	private ServerFactory() {};
 
+	/**
+	 * Function which will content module call to get the instance of the ClientIP
+	 * to use the function defined in that class.
+	 * 
+	 * @return instance of class ClientIP
+	 */
 	public static ClientIP getIPHandler() {
 		return clientIP;
 	}
--- src/processing/server/board/StopRequestHandler.java
+++ src/processing/server/board/StopRequestHandler.java
@@ -18,21 +18,51 @@ import processing.server.main.Requests;
 
 public class StopRequestHandler implements INotificationHandler {
 	
+	/**
+	 * onMessageRecived function of INotificationHandler which will be called
+	 * by the networking module whenever any message they have to send here with
+	 * this particular identifier to stop the connection from a particular client. 
+	 */
 	public void onMessageReceived(String message) {
 		
+		/**
+		 * As the client is closing it's application we need to remove it from 
+		 * the list of all the clients connected to this board.
+		 */
 		ClientBoardState.users.remove(new Username(message));
 		
+		/**
+		 * If after leaving this client user list becomes empty i.e no client is connected
+		 * to this board server, we need to close this server and inform the main server as
+		 * well.
+		 */
 		if(ClientBoardState.users.isEmpty()) {
 			
+			/**
+			 * As this Board Server is going to shut, we need to save the data of this server
+			 * for persistence, thus serializing the maps in ClientBoardState
+			 */
 			try {
 				String persistence = Serialize.serialize(ClientBoardState.maps);
 			} catch (IOException e) {
 				// log the exception
 			}
 			
+			/**
+			 * Saving the serialized the data in the file with name same as the BoardID.
+			 */
 			// save the persistence file
 			
+			/**
+			 * Notifying the networking module to stop listening for this server
+			 * messages by calling their stop function.
+			 */
 			ClientBoardState.communicator.stop();
+			
+			/**
+			 * Notifying the main server to shut this board's server by passing board ID as
+			 * the argument.
+			 */
 			Requests.removeBoardServer(ClientBoardState.boardId);
 		}
 	}
