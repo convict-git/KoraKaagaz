ffcf67f1244815500801104bd6c24e4ede456cb0
Navaneeth M Nambiar <nmnambiar@phoenix>
2020-11-08 16:48:07 +0530

    initialise console logger

--- src/infrastructure/validation/logger/ConsoleLogger.java
+++ src/infrastructure/validation/logger/ConsoleLogger.java
@@ -2,6 +2,7 @@ package infrastructure.validation.logger;
 
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
+import java.io.Console;
 
 /**
  * ConsoleLogger class that is part of loggerManager,
@@ -15,12 +16,23 @@ public class ConsoleLogger implements ILogger {
 	/** stores the format specified for the time-stamp */
 	private DateTimeFormatter timeStampFormat;
 	
+	/** stores a reference to the console object 
+	 *  If null, then no logs should be enabled 
+	 */
+	private static Console console;
+	
 	/**
 	 * 
 	 */
 	protected ConsoleLogger() {
 		
 		timeStampFormat = DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm:ss");
+		
+		console = System.console();
+		
+		if(console == null) {
+			// Houston, we need to disable all the log level filters and have no messages
+		}
 	}
 
 	@Override
