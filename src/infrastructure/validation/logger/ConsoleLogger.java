fbb27e9b63efde8b29c738375a7d2ed7dbed8979
Navaneeth M Nambiar <nmnambiar@phoenix>
2020-11-09 02:03:31 +0530

    synchronize public methods of console logger

--- src/infrastructure/validation/logger/ConsoleLogger.java
+++ src/infrastructure/validation/logger/ConsoleLogger.java
@@ -50,7 +50,7 @@ public class ConsoleLogger implements ILogger {
 	}
 
 	@Override
-	public void log(ModuleID moduleIdentifier, LogLevel level, String message) {
+	synchronized public void log(ModuleID moduleIdentifier, LogLevel level, String message) {
 		
 		LocalDateTime now = LocalDateTime.now();
 		String formatDateTime = now.format(timeStampFormat);
