818b347b614eb14eea6b58b9d33368b13a8bf705
sravand123 <34466670+sravand123@users.noreply.github.com>
2020-11-10 16:08:05 +0530

    Fixed Indentation

--- src/networking/LanCommunicator.java
+++ src/networking/LanCommunicator.java
@@ -15,7 +15,7 @@ import networking.queueManagement.*;
 import networking.utility.*;
 import infrastructure.validation.logger.*;
 
-public class LanCommunicator implements ICommunicator{
+public class LanCommunicator implements ICommunicator {
 
     /** SendQueueListener that will be listening on the send Queue */
     private SendQueueListener sendQueueListener;
@@ -34,23 +34,23 @@ public class LanCommunicator implements ICommunicator{
     private Thread contentReceiveQueueListenerWorker;
 
     /** Send Queue that has the message that should be sent across the network */
-    private IQueue<OutgoingPacket> sendQueue;
+    private IQueue < OutgoingPacket > sendQueue;
 
     /** Receive Queue in which we have the messages that needs to be sent to the processing module */
-    private IQueue<IncomingPacket> processingReceiveQueue;
+    private IQueue < IncomingPacket > processingReceiveQueue;
 
     /** Receive queue in which we have the messages that needs to be sent to the content module */
-    private IQueue<IncomingPacket> contentReceiveQueue;
+    private IQueue < IncomingPacket > contentReceiveQueue;
 
     /** Hashmap contains the handlers for the respective modules processing and content */
-    private HashMap<String, INotificationHandler> handlerMap;
-    
+    private HashMap < String, INotificationHandler > handlerMap;
+
     /** Port Number that we will be listening on the network */
     private int portNumber;
-    
+
     /** Boolean check variable for listeners to use */
     private static boolean isRunning = false;
-    
+
     /** Logger for logging errors and activities */
     ILogger logger = LoggerFactory.getLoggerInstance();
 
@@ -61,9 +61,9 @@ public class LanCommunicator implements ICommunicator{
      */
     public LanCommunicator(int port) {
         portNumber = port;
-        handlerMap = new HashMap<>();
+        handlerMap = new HashMap < > ();
     }
-    
+
     /**
      * This method is used to set the status
      * 
@@ -72,7 +72,7 @@ public class LanCommunicator implements ICommunicator{
     private void setStatus(boolean status) {
         isRunning = status;
     }
-    
+
     /**
      * This method is used by threads for getting the status
      * 
@@ -88,87 +88,83 @@ public class LanCommunicator implements ICommunicator{
      * processingReceiveQueueListener and contentReceiveQueueListener 
      */
     @Override
-    public void start(){
-        if(!getStatus()) {
-                
-                setStatus(true);
-                /** 
-                 * Initializing the queues required for the networking module
-                 * Send Queue - 1 with OutgoingPacket object in it.
-                 * Receive queue - 2 (processing and content) with IncomingPacket objects in it. 
-                 */
-                sendQueue = new ConcurrentBlockingQueue<OutgoingPacket>();
-                processingReceiveQueue = new ConcurrentBlockingQueue<IncomingPacket>();
-                contentReceiveQueue = new ConcurrentBlockingQueue<IncomingPacket>();
-                
-                logger.log(ModuleID.NETWORKING, LogLevel.INFO, "1 sendQueue and 2 receive queues created");
-        
-                /** 
-                 * The listener which listens on the send queue and transfer the messages on the lan network to the destination IP
-                 * A thread is spawn to perform this continuously whenever we have packets in the queue 
-                 */
-                sendQueueListener = new SendQueueListener(sendQueue);
-                sendQueueListenerWorker = new Thread(sendQueueListener);
-            try{
+    public void start() {
+        if (!getStatus()) {
+
+            setStatus(true);
+            /** 
+             * Initializing the queues required for the networking module
+             * Send Queue - 1 with OutgoingPacket object in it.
+             * Receive queue - 2 (processing and content) with IncomingPacket objects in it. 
+             */
+            sendQueue = new ConcurrentBlockingQueue < OutgoingPacket > ();
+            processingReceiveQueue = new ConcurrentBlockingQueue < IncomingPacket > ();
+            contentReceiveQueue = new ConcurrentBlockingQueue < IncomingPacket > ();
+
+            logger.log(ModuleID.NETWORKING, LogLevel.INFO, "1 sendQueue and 2 receive queues created");
+
+            /** 
+             * The listener which listens on the send queue and transfer the messages on the lan network to the destination IP
+             * A thread is spawn to perform this continuously whenever we have packets in the queue 
+             */
+            sendQueueListener = new SendQueueListener(sendQueue);
+            sendQueueListenerWorker = new Thread(sendQueueListener);
+            try {
                 sendQueueListenerWorker.start();
-            }
-            catch(Exception e){
-                logger.log(ModuleID.NETWORKING, LogLevel.ERROR, "sendQueueListenerWorker is not able to start "+ e.toString());  
+            } catch (Exception e) {
+                logger.log(ModuleID.NETWORKING, LogLevel.ERROR, "sendQueueListenerWorker is not able to start " + e.toString());
                 return;
             }
-                
-                logger.log(ModuleID.NETWORKING, LogLevel.INFO, "sendQueueListener thread started");
-                
-                /** 
-                 * The listener that will be listening on the network and that receives the packet sent by the sendQueueListener
-                 * This listener will distingush between processing module message and content module's message and push into their respective queues 
-                 */
-                socketListener = new SocketListener(portNumber, processingReceiveQueue, contentReceiveQueue);
-                socketListenerWorker = new Thread(socketListener);
-            try{
+
+            logger.log(ModuleID.NETWORKING, LogLevel.INFO, "sendQueueListener thread started");
+
+            /** 
+             * The listener that will be listening on the network and that receives the packet sent by the sendQueueListener
+             * This listener will distingush between processing module message and content module's message and push into their respective queues 
+             */
+            socketListener = new SocketListener(portNumber, processingReceiveQueue, contentReceiveQueue);
+            socketListenerWorker = new Thread(socketListener);
+            try {
                 socketListenerWorker.start();
-            }
-            catch(Exception e){
-                logger.log(ModuleID.NETWORKING, LogLevel.ERROR, "socketListenerWorker is not able to start "+ e.toString());  
+            } catch (Exception e) {
+                logger.log(ModuleID.NETWORKING, LogLevel.ERROR, "socketListenerWorker is not able to start " + e.toString());
                 return;
             }
-        
-                logger.log(ModuleID.NETWORKING, LogLevel.INFO, "socketListener thread started");
-                
-                /** 
-                 * This listener will be listening on the receive queue which is for the processing modules message
-                 * It will send the message which is pushed by network listener through the processing module handler 
-                 */
-                processingReceiveQueueListener = new ReceiveQueueListener(processingReceiveQueue, handlerMap);
-                processingReceiveQueueListenerWorker = new Thread(processingReceiveQueueListener);
-                
-            try{
+
+            logger.log(ModuleID.NETWORKING, LogLevel.INFO, "socketListener thread started");
+
+            /** 
+             * This listener will be listening on the receive queue which is for the processing modules message
+             * It will send the message which is pushed by network listener through the processing module handler 
+             */
+            processingReceiveQueueListener = new ReceiveQueueListener(processingReceiveQueue, handlerMap);
+            processingReceiveQueueListenerWorker = new Thread(processingReceiveQueueListener);
+
+            try {
                 processingReceiveQueueListenerWorker.start();
-            }
-            catch(Exception e){
-                logger.log(ModuleID.NETWORKING, LogLevel.ERROR, "processingReceiveQueueListenerWorker is not able to start "+ e.toString());  
+            } catch (Exception e) {
+                logger.log(ModuleID.NETWORKING, LogLevel.ERROR, "processingReceiveQueueListenerWorker is not able to start " + e.toString());
                 return;
             }
-                
-                logger.log(ModuleID.NETWORKING, LogLevel.INFO, "processingReceiveQueueListener thread started");
-                
-                /** 
-                 * This listener will be listening on the receive queue which is for the content modules message
-                 * It will send the message which is pushed by network listener through the content module handler 
-                 */
-                contentReceiveQueueListener = new ReceiveQueueListener(contentReceiveQueue, handlerMap);
-                contentReceiveQueueListenerWorker = new Thread(contentReceiveQueueListener);
-            try{
-                   contentReceiveQueueListenerWorker.start();
+
+            logger.log(ModuleID.NETWORKING, LogLevel.INFO, "processingReceiveQueueListener thread started");
+
+            /** 
+             * This listener will be listening on the receive queue which is for the content modules message
+             * It will send the message which is pushed by network listener through the content module handler 
+             */
+            contentReceiveQueueListener = new ReceiveQueueListener(contentReceiveQueue, handlerMap);
+            contentReceiveQueueListenerWorker = new Thread(contentReceiveQueueListener);
+            try {
+                contentReceiveQueueListenerWorker.start();
+            } catch (Exception e) {
+                logger.log(ModuleID.NETWORKING, LogLevel.ERROR, "contentReceiveQueueListener is not able to start");
+                return;
             }
-            catch(Exception e){
-                logger.log(ModuleID.NETWORKING, LogLevel.ERROR, "contentReceiveQueueListener is not able to start");  
-                return; 
-            }
-                
-                logger.log(ModuleID.NETWORKING, LogLevel.INFO, "contentReceiveQueueListener thread started");
-                logger.log(ModuleID.NETWORKING, LogLevel.INFO, "Communication is started");
-            
+
+            logger.log(ModuleID.NETWORKING, LogLevel.INFO, "contentReceiveQueueListener thread started");
+            logger.log(ModuleID.NETWORKING, LogLevel.INFO, "Communication is started");
+
         }
     }
 
@@ -178,7 +174,7 @@ public class LanCommunicator implements ICommunicator{
      * and also changing all the instances to null
      */
     @Override
-    public void stop(){
+    public void stop() {
         setStatus(false);
         socketListener.stop();
         sendQueueListener = null;
@@ -197,8 +193,8 @@ public class LanCommunicator implements ICommunicator{
         logger.log(ModuleID.NETWORKING, LogLevel.INFO, "Communication is stopped");
     }
 
-  
-    
+
+
     /**
      * This method creates a object with the params
      * and enqueues this object into the sendQueue
@@ -207,18 +203,18 @@ public class LanCommunicator implements ICommunicator{
      * @param identifier specifies who want to send it
      */
     @Override
-    public void send(String destination, String message, String identifier){
+    public void send(String destination, String message, String identifier) {
         /** Spliting the destination into ip and port */
         String[] dest = destination.split(":");
         /** checking if we have the destination in the required pattern ip:port */
-        if(dest.length != 2 || dest[0] == "" || dest[1] == "") {
-                logger.log(ModuleID.NETWORKING, LogLevel.WARNING, "Invalid destination : " + destination);
-                return;
+        if (dest.length != 2 || dest[0] == "" || dest[1] == "") {
+            logger.log(ModuleID.NETWORKING, LogLevel.WARNING, "Invalid destination : " + destination);
+            return;
         }
         /** checking if identifier is empty */
-        if(identifier == "") {
-                logger.log(ModuleID.NETWORKING, LogLevel.WARNING, "Empty identifier");
-                return;
+        if (identifier == "") {
+            logger.log(ModuleID.NETWORKING, LogLevel.WARNING, "Empty identifier");
+            return;
         }
         /** Creating the object for the outgoing packet that is being pushed into the send queue. */
         OutgoingPacket packet = new OutgoingPacket(destination, message, identifier);
@@ -227,37 +223,34 @@ public class LanCommunicator implements ICommunicator{
     }
 
     /**
-    * This method used to implement Publisher-Subscriber pattern. This method takes two arguments Identifier and handler. Based on the Identifier,
-    * the provided handler gets the data accordingly. The handler is the object of the class which implements INotificationHandler interface.
-    * Upon receiving the message through network, the networking module transfers the received message to subscribed module by invoking the onMessageReceived method
-    * of the provided handler object.
-    *
-    * @param identifier specifies the module subscribing.
-    *
-    * @param handler should handle message transfered from networking module.
-    */
+     * This method used to implement Publisher-Subscriber pattern. This method takes two arguments Identifier and handler. Based on the Identifier,
+     * the provided handler gets the data accordingly. The handler is the object of the class which implements INotificationHandler interface.
+     * Upon receiving the message through network, the networking module transfers the received message to subscribed module by invoking the onMessageReceived method
+     * of the provided handler object.
+     *
+     * @param identifier specifies the module subscribing.
+     *
+     * @param handler should handle message transfered from networking module.
+     */
     @Override
-    public void subscribeForNotifications(String identifier, INotificationHandler handler){
+    public void subscribeForNotifications(String identifier, INotificationHandler handler) {
 
         /** validating the handler */
-        if(handler==null)
-                logger.log(ModuleID.NETWORKING, LogLevel.WARNING, "Provide a valid handler");
+        if (handler == null)
+            logger.log(ModuleID.NETWORKING, LogLevel.WARNING, "Provide a valid handler");
 
         /** validating the identifier */
-        else if(identifier=="" || identifier==null)
-                logger.log(ModuleID.NETWORKING, LogLevel.WARNING,"Provide a valid identifier");
+        else if (identifier == "" || identifier == null)
+            logger.log(ModuleID.NETWORKING, LogLevel.WARNING, "Provide a valid identifier");
 
-        else{
+        else {
 
-                /** logging the info as the handler might be overridden if the specified identifier exists */
-                if(handlerMap.containsKey(identifier))
-                        logger.log(ModuleID.NETWORKING,LogLevel.INFO,"Already have the specified identifier");
+            /** logging the info as the handler might be overridden if the specified identifier exists */
+            if (handlerMap.containsKey(identifier))
+                logger.log(ModuleID.NETWORKING, LogLevel.INFO, "Already have the specified identifier");
 
-                /** inserting the specified identifier and handler into the hashmap */
-                handlerMap.put(identifier,handler);
+            /** inserting the specified identifier and handler into the hashmap */
+            handlerMap.put(identifier, handler);
         }
     }
 }
-
-    
-
