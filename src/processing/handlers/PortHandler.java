ca011344c215c973e78cfca725fb8256b9364a68
recursed <42519921+himanshu2047@users.noreply.github.com>
2020-11-13 01:19:53 +0530

    Send the server port to content module

--- src/processing/handlers/PortHandler.java
+++ src/processing/handlers/PortHandler.java
@@ -1,10 +1,12 @@
 package processing.handlers;
 
 import networking.INotificationHandler;
+import infrastructure.content.*;
 import processing.ClientBoardState;
 import processing.server.board.IServerCommunication;
 import processing.server.board.ServerCommunication;
 import processing.utility.*;
+
 /**
  * This class handles Port number of the Board Server received from the server
  * 
@@ -18,6 +20,9 @@ public class PortHandler implements INotificationHandler{
 		public void onMessageReceived(String message) {
 			Port portNumber = new Port(Integer.parseInt(message));
 			ClientBoardState.portNumber = portNumber;
+			
+			// sending the server port to the content module on the client side
+			ContentFactory.getServerPort().sendPort(portNumber.port);
 			
 			IServerCommunication communicator = new ServerCommunication();
 			communicator.getBoardState();
