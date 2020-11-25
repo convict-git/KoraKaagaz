92774f0c33c5efce5ab9883c5d937fb2e5b65473
DSR <rathoredevansh08@gmail.com>
2020-11-25 13:28:57 +0530

    indentation issues resolved

--- src/UI/Brush.java
+++ src/UI/Brush.java
@@ -1,6 +1,6 @@
 /**
- *Author: Anish Jain
- *This handles the brush and eraser mouse events for canavs.fxml
+ * Author: Anish Jain
+ * This handles the brush and eraser mouse events for canavs.fxml
  */
 
 package UI;
@@ -16,97 +16,97 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 /**
- *This deals with the brush class that has both brush and eraser.
- *It has functions that are called by the canvas controller.
- *These functions draw and erase in mouse drag and release conditions
+ * This deals with the brush class that has both brush and eraser.
+ * It has functions that are called by the canvas controller.
+ * These functions draw and erase in mouse drag and release conditions
  */
 public class Brush{
 
 	static boolean brushSelected = false;
-	
+
 	static boolean erasorSelected = false;
-	
+
 	static boolean sizeSelected =false;
-	
+
 	private static IDrawErase drawerase = ProcessingFactory.getProcessor();
-	
+
 	private static ArrayList<Pixel> pixels=new ArrayList<Pixel>();
-	
+
 	private static ArrayList<Position> position =new ArrayList<Position>();
-	
+
 	/**
-	 *To set them to default boolean value as false
+	 * To set them to default boolean value as false
 	 */
 	public static void defaultSelected() {
 		brushSelected = false;
 		erasorSelected = false;
 	}
-	
+
 	/**
-	 *This is called on mouse release for brush to send all the pixels as an arraylist to the processing module
-	 *@param color   any color selected
-	 *@param g   context 
-	 *@param x12 start pt(X)
-	 *@param y12 start pt(Y)
-	 *@param x22 end(dragged) pt(X)
-	 *@param y22 end(dragged) pt(Y)
-	 *@returns nothing
+	 * This is called on mouse release for brush to send all the pixels as an arraylist to the processing module
+	 * @param color   any color selected
+	 * @param g   context
+	 * @param x12 start pt(X)
+	 * @param y12 start pt(Y)
+	 * @param x22 end(dragged) pt(X)
+	 * @param y22 end(dragged) pt(Y)
+	 * @returns nothing
 	 */
 	public static void drawBrush(Color color,GraphicsContext g, double x12, double y12, double x22, double y22) {
-	    	g.setStroke(color);
-	    	g.stroke();
+		g.setStroke(color);
+		g.stroke();
 		g.closePath();
-		
+
 		drawerase.drawCurve(pixels);
 		pixels=new ArrayList<Pixel>();
     	}
-	
+
 	/**
-	 *This is called on mouse release for eraser to send all positions as an arraylist to the processing module
-	 *@param color   any color selected
-	 *@param g   context 
-	 *@param x12 start pt(X)
-	 *@param y12 start pt(Y)
-	 *@param x22 end(dragged) pt(X)
-	 *@param y22 end(dragged) pt(Y)
-	 *@returns nothing
+	 * This is called on mouse release for eraser to send all positions as an arraylist to the processing module
+	 * @param color   any color selected
+	 * @param g   context
+	 * @param x12 start pt(X)
+	 * @param y12 start pt(Y)
+	 * @param x22 end(dragged) pt(X)
+	 * @param y22 end(dragged) pt(Y)
+	 * @returns nothing
 	 */
 	public static void drawEraser(Color color,GraphicsContext g, double x12, double y12, double x22, double y22) {
-	    	g.stroke();
+		g.stroke();
 		g.closePath();
-		
-		drawerase.erase(position); 
+
+		drawerase.erase(position);
 		position=new ArrayList<Position>();
     	}
-	
+
 	/**
-	 *It is to show the brush effect while mouse drag and collect data to send to processing team
-	 *@param canvas  canvas to do on
-	 *@param color   any color selected
-	 *@param g   context 
-	 *@param x12 start pt(X)
-	 *@param y12 start pt(Y)
-	 *@param x22 end(dragged) pt(X)
-	 *@param y22 end(dragged) pt(Y)
-	 *@param size brushsize
-	 *@returns nothing
+	 * It is to show the brush effect while mouse drag and collect data to send to processing team
+	 * @param canvas  canvas to do on
+	 * @param color   any color selected
+	 * @param g   context
+	 * @param x12 start pt(X)
+	 * @param y12 start pt(Y)
+	 * @param x22 end(dragged) pt(X)
+	 * @param y22 end(dragged) pt(Y)
+	 * @param size brushsize
+	 * @returns nothing
 	 */
 	public static void drawBrushEffect(
 		Canvas canvasB,
 		Color color,
-		GraphicsContext g, 
+		GraphicsContext g,
 		double x12, double y12, double x22, double y22,double size
 	) {
 		double x = x22 - size / 2;
 		double y = y22 - size / 2;
-		
+
 		g.setFill(color);
 		g.fillRect(x, y, size, size);
-		
+
 		Intensity i = new Intensity((int) color.getRed(),(int) color.getGreen(),(int) color.getBlue());
-		for (int j = (int)y; j < (int)(y+size); j+=2) 
+		for (int j = (int)y; j < (int)(y+size); j+=2)
 		{
-			for (int k = (int)x; k < (int)(x+size); k+=2) 
+			for (int k = (int)x; k < (int)(x+size); k+=2)
 		    	{
 				Position  start = new Position((int) (j*100),(int) (k*100));
 				Pixel p1 = new Pixel(start,i);
@@ -114,38 +114,38 @@ public class Brush{
 		    	}
 		}
 	}
-	
+
 	/**
-	 *It is to show the erase effect while mouse drag and collect data to send to processing team
-	 *@param canvas  canvas to do on
-	 *@param color   any color selected
-	 *@param g   context 
-	 *@param x12 start pt(X)
-	 *@param y12 start pt(Y)
-	 *@param x22 end(dragged) pt(X)
-	 *@param y22 end(dragged) pt(Y)
-	 *@param size brushsize
-	 *@returns nothing
+	 * It is to show the erase effect while mouse drag and collect data to send to processing team
+	 * @param canvas  canvas to do on
+	 * @param color   any color selected
+	 * @param g   context
+	 * @param x12 start pt(X)
+	 * @param y12 start pt(Y)
+	 * @param x22 end(dragged) pt(X)
+	 * @param y22 end(dragged) pt(Y)
+	 * @param size brushsize
+	 * @returns nothing
 	 */
 	public static void drawEraserEffect(
 		Canvas canvasB,
 		Color color,
-		GraphicsContext g, 
+		GraphicsContext g,
 		double x12, double y12, double x22, double y22,double size
 	) {
 		double x = x22 - size / 2;
 		double y = y22 - size / 2;
-		
+
 		g.clearRect(x, y, size, size);
-		
-		for (int j = (int)y; j < (int)(y+size); j+=2) 
+
+		for (int j = (int)y; j < (int)(y+size); j+=2)
 		{
-			for (int k = (int)x; k < (int)(x+size); k+=2) 
+			for (int k = (int)x; k < (int)(x+size); k+=2)
 		    {
 				Position  start = new Position((int) (j*100),(int) (k*100));
 				position.add(start);
 		    }
 		}
 	}
-	
+
 }
