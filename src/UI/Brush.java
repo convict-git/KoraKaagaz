75c64427b5a0a8bf22446a772e0a4c60f605eb2c
DSR <rathoredevansh08@gmail.com>
2020-12-02 11:08:06 +0530

    Brush.java: convention updates

--- src/UI/Brush.java
+++ src/UI/Brush.java
@@ -46,7 +46,7 @@ public class Brush{
 	/**
 	 *This is called on mouse release for brush to send all the pixels as an arraylist to the processing module
 	 *@param color   any color selected
-	 *@param g   context 
+	 *@param g   context
 	 *@param startx start pt(X)
 	 *@param starty start pt(Y)
 	 *@param endx end(dragged) pt(X)
@@ -65,7 +65,7 @@ public class Brush{
 	/**
 	 *This is called on mouse release for eraser to send all positions as an arraylist to the processing module
 	 *@param color   any color selected
-	 *@param g   context 
+	 *@param g   context
 	 *@param startx start pt(X)
 	 *@param starty start pt(Y)
 	 *@param endx end(dragged) pt(X)
@@ -83,7 +83,7 @@ public class Brush{
 	 *It is to show the brush effect while mouse drag and collect data to send to processing team
 	 *@param canvas  canvas to do on
 	 *@param color   any color selected
-	 *@param g   context 
+	 *@param g   context
 	 *@param startx start pt(X)
 	 *@param starty start pt(Y)
 	 *@param endx end(dragged) pt(X)
@@ -94,17 +94,17 @@ public class Brush{
 	public static void drawBrushEffect(
 		Canvas canvasB,
 		Color color,
-		GraphicsContext g, 
+		GraphicsContext g,
 		double startx, double starty, double endx, double endy,double size
 	) {
 		g.stroke();
 		g.setLineCap(StrokeLineCap.ROUND);
-		g.setLineWidth(size);	
+		g.setLineWidth(size);
 		g.setStroke(color);
 		g.strokeLine(startx,starty,endx,endy);
-		
+
 		double x=0,y=0,m=0,flag=0;
-		
+
 		/**
 		 * Check if slope is 0 or infinity.
 		 * If either is there flag to a certain value
@@ -120,42 +120,45 @@ public class Brush{
 			flag=0;
 			m= (endy-starty)/(endx-startx);
 		}
-		
+
 		/**
 		 * Intensity in terms of RGB format
 		 */
 		Intensity i = new Intensity((int) color.getRed(),(int) color.getGreen(),(int) color.getBlue());
-		
+
 		/**
-		 * The calculation of pixels 
+		 * The calculation of pixels
 		 * Outer loop is for every 2 distance for line drawn from (startx , starty) to (endx , endy)
 		 * Inner loop is half the size on both sides of line with distance 1.
 		 * j and k are local loop variables.
 		 */
-		for (int j = 0; j <= (int)(Math.sqrt((endy - starty) * (endy - starty) + (endx - startx) * (endx - startx))); j+=2) 
-		{
-			for (int k = (int)(-1 * size/2); k <= (int)(size/2); k+=1) 
+		for (
+			int j = 0;
+			j <= (int)(Math.sqrt((endy - starty) * (endy - starty) + (endx - startx) * (endx - startx)));
+			j+=2
+		) {
+			for (int k = (int)(-1 * size/2); k <= (int)(size/2); k+=1)
 		    {
-				if(flag==0) {
-					x = startx + Math.sqrt((j*j)/(m * m + 1)) + ( Math.abs(m * k) * ( Math.sqrt(1/(1 + m * m)) ) ) ;
+				if (flag==0) {
+					x = startx + Math.sqrt((j*j)/(m * m + 1)) + ( Math.abs(m * k) * ( Math.sqrt(1/(1 + m * m)) ));
 					y = starty + ( Math.abs(m) * Math.sqrt((j*j)/(m * m + 1))) - ( (1/m) * Math.abs(m * k) * Math.sqrt(1/(1 + m * m)));
 				}
-				else if(flag==1) {
+				else if (flag==1) {
 					x = startx + k;
 					y = starty + j;
 				}
-				else if(flag==1) {
+				else if (flag==1) {
 					x = startx + j;
 					y = starty + k;
 				}
-					
+
 				/**
-				 * start point for position 
+				 * start point for position
 				 */
 				Position  start = new Position((int) (x*100),(int) (y*100));
 				Pixel p1 = new Pixel(start,i);
 				pixels.add(p1);
-		    }
+			}
 		}
 	}
 
@@ -163,7 +166,7 @@ public class Brush{
 	 *It is to show the erase effect while mouse drag and collect data to send to processing team
 	 *@param canvas  canvas to do on
 	 *@param color   any color selected
-	 *@param g   context 
+	 *@param g   context
 	 *@param startx start pt(X)
 	 *@param starty start pt(Y)
 	 *@param endx end(dragged) pt(X)
@@ -174,63 +177,65 @@ public class Brush{
 	public static void drawEraserEffect(
 		Canvas canvasB,
 		Color color,
-		GraphicsContext g, 
+		GraphicsContext g,
 		double startx, double starty, double endx, double endy,double size
-	) {	
+	) {
 		g.stroke();
 		g.setLineCap(StrokeLineCap.ROUND);
 		g.setLineWidth(size);
 		g.setStroke(Color.WHITE);
 		g.strokeLine(startx,starty,endx,endy);
-		
+
 		/**
 		 * Check if slope is 0 or infinity.
 		 * If either is there flag to a certain value
 		 * m = slope
 		 */
 		double x=0,y=0,m=0,flag=0;
-		if(startx==endx) {
+		if (startx==endx) {
 			flag=1;
 		}
-		else if(endy==starty) {
+		else if (endy==starty) {
 			flag=2;
 		}
 		else {
 			flag=0;
 			m= (endy-starty)/(endx-startx);
 		}
-		
+
 		/**
-		 * The calculation of pixels 
+		 * The calculation of pixels
 		 * Outer loop is for every 2 distance for line drawn from (startx , starty) to (endx , endy)
 		 * Inner loop is half the size on both sides of line with distance 1.
 		 * j and k are local loop variables.
 		 */
-		for (int j = 0; j <= (int)(Math.sqrt((endy - starty) * (endy - starty) + (endx - startx) * (endx - startx))); j+=2) 
-		{
-			for (int k = (int)(-1 * size/2); k <= (int)(size/2); k+=1) 
-		    {
-				if(flag==0) {
-					x = startx + Math.sqrt((j*j)/(m * m + 1)) + ( Math.abs(m * k) * ( Math.sqrt(1/(1 + m * m)) ) ) ;
+		for (
+			int j = 0;
+			j <= (int)(Math.sqrt((endy - starty) * (endy - starty) + (endx - startx) * (endx - startx)));
+			j+=2
+		) {
+			for (int k = (int)(-1 * size/2); k <= (int)(size/2); k+=1)
+			{
+				if (flag==0) {
+					x = startx + Math.sqrt((j*j)/(m * m + 1)) + ( Math.abs(m * k) * ( Math.sqrt(1/(1 + m * m))));
 					y = starty + ( Math.abs(m) * Math.sqrt((j*j)/(m * m + 1))) - ( (1/m) * Math.abs(m * k) * Math.sqrt(1/(1 + m * m)));
 				}
-				else if(flag==1) {
+				else if (flag==1) {
 					x = startx + k;
 					y = starty + j;
 				}
-				else if(flag==1) {
+				else if (flag==1) {
 					x = startx + j;
 					y = starty + k;
 				}
-					
+
 				/**
-				 * start point for position 
+				 * start point for position
 				 */
 				Position  start = new Position((int) (x*100),(int) (y*100));
 				position.add(start);
-		    }
+			}
 		}
-	
-	}
 
+	}
 }
