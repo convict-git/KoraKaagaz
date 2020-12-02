c40f28865d8971b412937d246ae4865ceefc237e
Sajith Kumar <111701012@smail.iitpkd.ac.in>
2020-12-03 02:17:17 +0530

    Resolved NullPointerException Error

--- src/UI/PixelListener.java
+++ src/UI/PixelListener.java
@@ -1,14 +1,9 @@
 package UI;
 
 import java.util.ArrayList;
-
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
 import processing.*;
-import processing.utility.Intensity;
 import processing.utility.Pixel;
-import processing.utility.Position;
+
 
 /**
  * @author: Sajith Kumar Erasani
@@ -18,22 +13,27 @@ import processing.utility.Position;
 
 public class PixelListener implements IChanges {
 	/**
-	 * This method will update the canvas with given pixels
+	 * This method will call updateChanges method to update the canvas with given pixels
 	 * @param pixels ArrayList of pixels
 	 */
 	@Override
 	public void getChanges(ArrayList<Pixel> pixels) {
-		CanvasController controller = new CanvasController();
-		controller.updateChanges(pixels);
+		if (pixels!=null) {
+			CanvasController controller = new CanvasController();
+			controller.updateChanges(pixels);
+		}
 	}
+	
 
 	/**
-	 * This method will select an object by changing the color of given pixels
+	 * This method will call updateSelectedPixels method to select an object by changing the color of given pixels
 	 * @param pixels ArrayList of pixels
 	 */
 	@Override
 	public void giveSelectedPixels(ArrayList<Pixel> pixels) {
-		CanvasController controller = new CanvasController();
-		controller.updateSelectedPixels(pixels);
+		if (pixels!=null) {
+			CanvasController controller = new CanvasController();
+			controller.updateSelectedPixels(pixels);
+		}
 	}
 }
