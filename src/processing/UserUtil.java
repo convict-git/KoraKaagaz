4226e9c69fd8e8d220d5cd188d979768cfea3fed
Rakesh Kumar <31342754+iamrakesh28@users.noreply.github.com>
2020-12-06 03:58:49 +0530

    Undo Redo Bug-Fixes

--- src/processing/UserUtil.java
+++ src/processing/UserUtil.java
@@ -17,140 +17,140 @@ import infrastructure.validation.logger.*;
 
 public class UserUtil {
 	
-	// gets the logger instance
-	private static ILogger logger = LoggerFactory.getLoggerInstance();
+    // gets the logger instance
+    private static ILogger logger = LoggerFactory.getLoggerInstance();
 	
-	/**
-	 * UI calls this method to initiate connection 
-	 * with the server.
-	 * The information provided in the arguments
-	 * are used to start the connection
-	 * 
-	 * @param username Username
-	 * @param ipAddress Server's ip address
-	 * @param boardId Board ID	
-	 */
-	public static void initiateUserConnection(
-		String username, 
-		String ipAddress, 
-		String boardId
-	) {
+    /**
+     * UI calls this method to initiate connection 
+     * with the server.
+     * The information provided in the arguments
+     * are used to start the connection
+     * 
+     * @param username Username
+     * @param ipAddress Server's ip address
+     * @param boardId Board ID	
+     */
+    public static void initiateUserConnection(
+                                              String username, 
+                                              String ipAddress, 
+                                              String boardId
+                                              ) {
     		// stores the information
-		ClientBoardState.username  = new Username(username);
-		ClientBoardState.serverIp  = new IpAddress(ipAddress);
+        ClientBoardState.username  = new Username(username);
+        ClientBoardState.serverIp  = new IpAddress(ipAddress);
 
-		if (boardId == null) {
-		    ClientBoardState.boardId = null;
-		}
-		else {
-		    ClientBoardState.boardId = new BoardId(boardId);
-		}
+        if (boardId == null) {
+            ClientBoardState.boardId = null;
+        }
+        else {
+            ClientBoardState.boardId = new BoardId(boardId);
+        }
 		
-		// gets the client information (ip address + free port)
-		ClientInfo clientInfo = CommunicatorFactory.getClientInfo();
-		logger.log(
-				ModuleID.PROCESSING, 
-				LogLevel.SUCCESS,
-				"[#" + Thread.currentThread().getId() + "] "
-				+ "Got the client information"
-		);
+        // gets the client information (ip address + free port)
+        ClientInfo clientInfo = CommunicatorFactory.getClientInfo();
+        logger.log(
+                   ModuleID.PROCESSING, 
+                   LogLevel.SUCCESS,
+                   "[#" + Thread.currentThread().getId() + "] "
+                   + "Got the client information"
+                   );
     	
-		// stores the user ip and port number
-		ClientBoardState.userIP  = new IpAddress(clientInfo.getIp());
-		Port userPort  = new Port(clientInfo.getPort());
-		ClientBoardState.userPort = userPort;
+        // stores the user ip and port number
+        ClientBoardState.userIP  = new IpAddress(clientInfo.getIp());
+        Port userPort  = new Port(clientInfo.getPort());
+        ClientBoardState.userPort = userPort;
 
-		// Initialize the data members of the ClientBoardState
-		ClientBoardState.maps = new BoardState();
-		ClientBoardState.undoStack = new ArrayList <BoardObject>();
-		ClientBoardState.redoStack = new ArrayList <BoardObject>();
-		ClientBoardState.userId = new UserId(
-						     ClientBoardState.userIP,
-						     ClientBoardState.username
-						     );
+        // Initialize the data members of the ClientBoardState
+        ClientBoardState.maps = new BoardState();
+        ClientBoardState.undoStack = new ArrayList <BoardObject>();
+        ClientBoardState.redoStack = new ArrayList <BoardObject>();
+        ClientBoardState.userId = new UserId(
+                                             ClientBoardState.userIP,
+                                             ClientBoardState.username
+                                             );
     	
-		// gets a communicator and starts it
-		ClientBoardState.communicator = CommunicatorFactory.getCommunicator(userPort.port);
-		ClientBoardState.communicator.start();
-		// starts the client side processing
-		ClientBoardState.start();
+        // gets a communicator and starts it
+        ClientBoardState.communicator = CommunicatorFactory.getCommunicator(userPort.port);
+        ClientBoardState.communicator.start();
+        // starts the client side processing
+        ClientBoardState.start();
     	
-		logger.log(
-				ModuleID.PROCESSING, 
-				LogLevel.SUCCESS, 
-				"[#" + Thread.currentThread().getId() + "] "
-				+ "Started communicator and the ClientBoardState"
-		);
-		return;
-	}
+        logger.log(
+                   ModuleID.PROCESSING, 
+                   LogLevel.SUCCESS, 
+                   "[#" + Thread.currentThread().getId() + "] "
+                   + "Started communicator and the ClientBoardState"
+                   );
+        return;
+    }
     
-	/**
-     	 * Finds the username of the objects drawn at these list of positions
-     	 * 
-     	 * Since, there could be multiple objects at these positions drawn by different
-     	 * users so username of the user having maximum occurrences at these positions
-     	 * is selected
-     	 * 
-     	 * @param positions list of positions
-     	 * @return username
-     	 * @return null if there is no object present at those positions
-    	 */
+    /**
+     * Finds the username of the objects drawn at these list of positions
+     * 
+     * Since, there could be multiple objects at these positions drawn by different
+     * users so username of the user having maximum occurrences at these positions
+     * is selected
+     * 
+     * @param positions list of positions
+     * @return username
+     * @return null if there is no object present at those positions
+     */
     
-    	public static String getUser(ArrayList <Position> positions) {
+    public static String getUser(ArrayList <Position> positions) {
     	
     		// null object
     		if (positions == null) {
-    			logger.log(
-    					ModuleID.PROCESSING, 
-    					LogLevel.WARNING, 
-    					"[#" + Thread.currentThread().getId() + "] "
-    					+ "Got a null ArrayList in getUser"
-    			);
-    			return null;
+            logger.log(
+                       ModuleID.PROCESSING, 
+                       LogLevel.WARNING, 
+                       "[#" + Thread.currentThread().getId() + "] "
+                       + "Got a null ArrayList in getUser"
+                       );
+            return null;
     		}
     	
     		// gets the id of the object occurring maximum times at these positions
     		ObjectId objectId = ClientBoardState
-    						.maps
-    						.getMostProbableObjectId(positions);
+            .maps
+            .getMostProbableObjectId(positions);
     	
     		String maxOccUser = null;
     	
     		// if a object found
     		if (objectId != null) {
-    			// gets the username for the above object
-    			maxOccUser = ClientBoardState
+            // gets the username for the above object
+            maxOccUser = ClientBoardState
     						.maps
     						.getUserNameOfObject(objectId);
     		}
     	
     		logger.log(
-    				ModuleID.PROCESSING, 
-    				LogLevel.SUCCESS, 
-    				"[#" + Thread.currentThread().getId() + "] "
-    				+ "Found the user at the selected positions"
-    		);
+                   ModuleID.PROCESSING, 
+                   LogLevel.SUCCESS, 
+                   "[#" + Thread.currentThread().getId() + "] "
+                   + "Found the user at the selected positions"
+                   );
     		return maxOccUser;
   	}
     
-    	/**
-     	* Stops the board session for the user
-     	*/
-    	public static void stopBoardSession() {
+    /**
+     * Stops the board session for the user
+     */
+    public static void stopBoardSession() {
     	
-  		IServerCommunication serverComm = new ServerCommunication();
+        IServerCommunication serverComm = new ServerCommunication();
     		// Informs the server about closing the session of the user
     		serverComm.stopConnection();
     	
     		// stops the communicator (all its threads)
     		ClientBoardState.communicator.stop();
     		logger.log(
-    				ModuleID.PROCESSING, 
-    				LogLevel.SUCCESS, 
-    				"[#" + Thread.currentThread().getId() + "] "
-    				+ "Server is informed and Communicator is stopped"
-    		);
+                   ModuleID.PROCESSING, 
+                   LogLevel.SUCCESS, 
+                   "[#" + Thread.currentThread().getId() + "] "
+                   + "Server is informed and Communicator is stopped"
+                   );
     		return;
-    	}
+    }
         
 }
